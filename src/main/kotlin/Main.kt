f7cfb10a344e4c7ae010890a452ca894f3dfba5b
margaret-ngugi <ngugiwairimumargaret@gmail.com>
2023-02-04 21:11:52 +0300

    https://github.com/margaret-ngugi/Adalab.git

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var name = "Carol"
+    println(name)
+    var age = 23
+    println(age)
+    var weight = 67
+    println(weight)
+    var number = 723456898
+    println(number)
+    var isKenyan = true
+println(isKenyan)
+
+}
